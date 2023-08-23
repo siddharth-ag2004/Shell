d05293a387c4f951b10c8f356a83ea395ed8a6a1
siddharth-ag2004 <sidag2020@gmail.com>
2023-08-24 00:22:42 +0530

    peek almost finished

--- peek.c
+++ peek.c
@@ -16,6 +16,7 @@ int cmpf_string(const void* a,const void* b)
 
 void permissions(char *filepath) 
 {
+    // printf("%s\n",filepath);
     struct stat fileStat;
     if (stat(filepath, &fileStat) == -1) 
     {
@@ -37,6 +38,7 @@ void permissions(char *filepath)
     printf((mode & S_IXOTH) ? "x" : "-");
     printf("\t");
 }
+
 void peek(char* flag1 ,char* flag2 ,char* path)
 {
     char** store_strings =  (char**)malloc(1000*sizeof(char*));         // GIVEN RANDON SIZE
@@ -77,9 +79,9 @@ void peek(char* flag1 ,char* flag2 ,char* path)
 
         closedir(dr);
     }
-    else if(flag1!=NULL && flag2==NULL)
+    else if(flag1!=NULL)
     {
-        if(strcmp(flag1,"-a")==0)
+        if(strcmp(flag1,"-a")==0 && flag2==NULL)
         {
             struct dirent *d;
             if(path==NULL)
@@ -108,18 +110,23 @@ void peek(char* flag1 ,char* flag2 ,char* path)
 
             closedir(dr);
         }
-        else if(strcmp(flag1,"-l")==0 || strcmp(flag1,"-la")==0 || strcmp(flag1,"-al")==0)
+        else if(strcmp(flag1,"-l")==0 || strcmp(flag1,"-la")==0 || strcmp(flag1,"-al")==0 || flag2!=NULL)
         {
             int a_flag=1;
             if(strcmp(flag1,"-l")==0)
             {
                 a_flag=0;
             }
+            if(flag2!=NULL)
+            {
+                a_flag=1;
+            }
             struct dirent *d;
             if(path==NULL)
             {
                 path = ".";
             }
+
             DIR *dr = opendir(path);
             if(dr==NULL)
             {
@@ -139,7 +146,10 @@ void peek(char* flag1 ,char* flag2 ,char* path)
 
             for (int i=0;i<idx;i++) 
             {
-                
+                char* absolute_path_string = (char*)malloc(sizeof(char)*PATH_MAX); 
+                sprintf(absolute_path_string, "%s/%s/%s", cwd, path, store_strings[i]);
+                // printf("%s\n",cwd);
+                // permissions(absolute_path_string);
                 permissions(store_strings[i]);
 
                 struct stat fileStat;
@@ -150,7 +160,7 @@ void peek(char* flag1 ,char* flag2 ,char* path)
                 }
                 
                 printf("%lu\t", (unsigned long)fileStat.st_nlink);
-
+                
                 char *username = getenv("USER");
                 printf("%s\t", username);
                 struct group *gr = getgrgid(fileStat.st_gid);
