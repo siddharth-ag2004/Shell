706ec0e926325a546fa959622fb8dbc4c4731696
siddharth-ag2004 <sidag2020@gmail.com>
2023-09-12 22:43:44 +0530

    neonate 0 handled

--- neonate.c
+++ neonate.c
@@ -3,6 +3,15 @@
 int x_flag = 0;
 int time_interval;
 
+int kbhit() 
+{
+    struct timeval tv = {0L, 0L};
+    fd_set fds;
+    FD_ZERO(&fds);
+    FD_SET(0, &fds);
+    return select(1, &fds, NULL, NULL, &tv);
+}
+
 void PrintPid()
 {
   char recent_path[PATH_MAX] = "/proc/loadavg";
@@ -17,7 +26,7 @@ void PrintPid()
   char strtest[PATH_MAX];
   fgets(strtest,PATH_MAX,file);
   int field_count = sscanf(strtest, "%*f %*f %*f %*s %s", pid);
-  
+  fclose(file);
   if (field_count == 1)
     printf("%s\n", pid);
   else
@@ -37,46 +46,45 @@ void neonate(char* time_str)
 {
   time_interval = atoi(time_str);
 
-  if(time_interval <= 0)
+  if(time_interval <= 0 && strcmp(time_str,"0")!=0)
   {
     perror("Please enter positive integer time interval");
     return;
   }
 
-  signal(SIGALRM,ALRMhandler);
-  alarm(time_interval);
-  PrintPid();
-  while(1)
+  if(strcmp(time_str,"0")==0)
   {
-    char c;
-    if (read(STDIN_FILENO, &c, 1) == 1) 
+    while(1)
     {
-      if (c == 'x') {
-          x_flag=1;
-          break;
+      PrintPid();
+      if(kbhit())
+      {
+        char c;
+        if (read(STDIN_FILENO, &c, 1) == 1) 
+        {
+          if (c == 'x') {
+              x_flag=1;
+              break;
+          }
+        }
       }
     }
-
-    // char recent_path[PATH_MAX] = "/proc/loadavg";
-    // FILE *file = fopen(recent_path, "r");
-    // if (file == NULL) 
-    // {
-    //   perror("Error opening /proc/loadavg");
-    //   return;
-    // }
-
-    // alarm(time_interval);
-    // char pid[PATH_MAX];
-    // char strtest[PATH_MAX];
-    // fgets(strtest,PATH_MAX,file);
-    // int field_count = sscanf(strtest, "%*f %*f %*f %*s %s", pid);
-    
-    // if (field_count == 1)
-    //   printf("%s\n", pid);
-    // else
-    //   perror("Error reading PID");
-
-    // // sleep(time_interval);
-    // fclose(file);
+  }
+  else
+  {
+    signal(SIGALRM,ALRMhandler);
+    alarm(time_interval);
+    PrintPid();
+    while(1)
+    {
+      char c;
+      if (read(STDIN_FILENO, &c, 1) == 1) 
+      {
+        if (c == 'x') {
+            x_flag=1;
+            break;
+        }
+      }
+    }
   }
 }
